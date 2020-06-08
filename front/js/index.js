5f3d31aa6dc27d878bd2a4ac5a8ca6290d6d3163
qiaopin <qiaopin@163.com>
2020-06-08 19:14:24 +0800

    index页的header、footer和联系我们页面

---
+++ front/js/index.js
@@ -0,0 +1,9 @@
+// let widthsb=document.body.clientWidth
+// console.log(widthsb)
+// if(widthsb<560){
+//     $(".short").show()
+//     $(".long").hide()
+// }else{
+//     $(".short").hide()
+//     $(".long").show()
+// }
