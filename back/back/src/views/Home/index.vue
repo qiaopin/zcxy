160e03aeebed5c032080e42926b89d80e351536f
qiaopin <qiaopin@163.com>
2020-06-17 17:31:29 +0800

    回到顶部的更新

--- back/back/src/views/Home/index.vue
+++ back/back/src/views/Home/index.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>Home</div>
+  <el-container>
+  <el-aside width="200px">
+    <!-- 侧边栏 -->
+    <el-row class="tac">
+      <el-col :span="12">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#409EFF">
+      <el-menu-item index="1">
+          <i class="el-icon-location"></i>
+          <span>公司产品</span>
+        </el-menu-item >
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">公司新闻</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-document"></i>
+        <span slot="title">行业新闻</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">用户管理</span>
+      </el-menu-item>
+        </el-menu>
+      </el-col>
+    </el-row>
+
+    <!-- 侧边栏完 -->
+  </el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
@@ -9,4 +46,29 @@
 </script>
 
 <style lang="less" scoped>
+.el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    // height:200px;
+  }
+  
+  .el-aside {
+    background-color: rgb(84, 92, 100);
+    color: #333;
+    text-align: center;
+    height: 730px;
+    .el-col-12{
+      width: 100%;
+      overflow: hidden;
+    }
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+  }
+  
+ 
 </style>
--- front/js/index.js
+++ front/js/index.js
@@ -9,67 +9,121 @@
 // }
 
 // nav
-$(".about").mousemove(function(){
-    $(".about ol").show()
-})
-$(".about").mouseout(function(){
-    $(".about ol").hide()
-})
-$(".work").mousemove(function(){
-    $(".work ol").show()
-})
-$(".work").mouseout(function(){
-    $(".work ol").hide()
-})
-$(".news").mousemove(function(){
-    $(".news ol").show()
-})
-$(".news").mouseout(function(){
-    $(".news ol").hide()
-})
-$(".us").mousemove(function(){
-    $(".us ol").show()
-})
-$(".us").mouseout(function(){
-    $(".us ol").hide()
-})
+
+window.onload=function(){
+    $(".about").mousemove(function(){
+        $(".about ol").show()
+    })
+    $(".about").mouseout(function(){
+        $(".about ol").hide()
+    })
+    $(".work").mousemove(function(){
+        $(".work ol").show()
+    })
+    $(".work").mouseout(function(){
+        $(".work ol").hide()
+    })
+    $(".news").mousemove(function(){
+        $(".news ol").show()
+    })
+    $(".news").mouseout(function(){
+        $(".news ol").hide()
+    })
+    $(".us").mousemove(function(){
+        $(".us ol").show()
+    })
+    $(".us").mouseout(function(){
+        $(".us ol").hide()
+    })
+    
+    
+    // 业务领域
+    $('.content-left>a:first-child').mouseover(function(){
+        $('a.dx>div').css('background-color','#6bd2ff')
+        $('.content-right>.dx').show()
+        $('.content-right>.jz').hide()
+        $('a.rj>div').css('background-color','rgba(173, 223, 248, 0.8)')
+        $('a.jz>div').css('background-color','rgba(173, 223, 248, 0.8)')
+        $('.content-right>.rj').hide()
+    })
+    
+    $('.content-left>a:first-child').mouseout(function(){
+        // $('a.dx>div').css('background-color','rgba(173, 223, 248, 0.8)')
+    })
+    
+    $('.content-left>a:nth-child(2)').mouseover(function(){
+        $('a.dx>div').css('background-color','rgba(173, 223, 248, 0.8)')
+        $('a.jz>div').css('background-color','#6bd2ff')
+        $('.content-right>.dx').hide()
+        $('.content-right>.jz').show()
+        $('a.rj>div').css('background-color','rgba(173, 223, 248, 0.8)')
+        $('.content-right>.rj').hide()
+    })
+    
+    $('.content-left>a:nth-child(2)').mouseout(function(){
+        // $('a.jz>div').css('background-color','rgba(173, 223, 248, 0.8)')
+    })
+    
+    $('.content-left>a:nth-child(3)').mouseover(function(){
+        $('a.dx>div').css('background-color','rgba(173, 223, 248, 0.8)')
+        $('a.jz>div').css('background-color','rgba(173, 223, 248, 0.8)')
+        $('a.rj>div').css('background-color','#6bd2ff')
+        $('.content-right>.dx').hide()
+        $('.content-right>.jz').hide()
+        $('.content-right>.rj').show()
+    })
+
+    // var returnBtn=document.createElement('a');
+    // "background":"url(../image/first-page/returntop.png)"
+    
+   if(location.href.indexOf('index.html')==-1){
+    $("body").append("<a>返回<br/>顶部</a>");
+    $('body>a').css({"width":" 1rem","height":" 1rem","color":"#fff","font-size":".2rem","text-align":"center","padding-top":".17rem","z-index":"998","position":"fixed","right":".25rem","bottom":"6rem","background-color":"#333","border-radius":"50%","background-size":"cover","display":"none"});
+    $('body>a').attr("id","return_top")
+   }
+
+   var obtn = document.getElementById('return_top');  //获取回到顶部按钮的ID
+   var clientHeight = document.documentElement.clientHeight;   //获取可视区域的高度
+   var timer = null; //定义一个定时器
+   var isTop = true; //定义一个布尔值，用于判断是否到达顶部
+
+   window.onscroll = function(){         //滚动条滚动事件
+
+       //获取滚动条的滚动高度
+       var osTop = document.documentElement.scrollTop || document.body.scrollTop; 
+
+       if(osTop >= clientHeight){  //如果滚动高度大于可视区域高度，则显示回到顶部按钮
+               obtn.style.display = 'block';
+       }else{         //否则隐藏
+           obtn.style.display = 'none';
+       }
+
+       //主要用于判断当 点击回到顶部按钮后 滚动条在回滚过程中，若手动滚动滚动条，则清除定时器
+       if(!isTop){     
+           clearInterval(timer);
+       }
+       isTop = false;
+   }
+
+   obtn.onclick = function(){    //回到顶部按钮点击事件
+       //设置一个定时器
+       timer = setInterval(function(){
+           //获取滚动条的滚动高度
+           var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+           //用于设置速度差，产生缓动的效果
+           var speed = Math.floor(-osTop / 6);
+           document.documentElement.scrollTop = document.body.scrollTop = osTop + speed;
+           isTop =true;  //用于阻止滚动事件清除定时器
+           if(osTop == 0){
+               clearInterval(timer);
+           }
+       },30);
+   }
 
 
-// 业务领域
-$('.content-left>a:first-child').mouseover(function(){
-    $('a.dx>div').css('background-color','#6bd2ff')
-    $('.content-right>.dx').show()
-    $('.content-right>.jz').hide()
-    $('a.rj>div').css('background-color','rgba(173, 223, 248, 0.8)')
-    $('a.jz>div').css('background-color','rgba(173, 223, 248, 0.8)')
-    $('.content-right>.rj').hide()
-})
 
-$('.content-left>a:first-child').mouseout(function(){
-    // $('a.dx>div').css('background-color','rgba(173, 223, 248, 0.8)')
-})
 
-$('.content-left>a:nth-child(2)').mouseover(function(){
-    $('a.dx>div').css('background-color','rgba(173, 223, 248, 0.8)')
-    $('a.jz>div').css('background-color','#6bd2ff')
-    $('.content-right>.dx').hide()
-    $('.content-right>.jz').show()
-    $('a.rj>div').css('background-color','rgba(173, 223, 248, 0.8)')
-    $('.content-right>.rj').hide()
-})
-
-$('.content-left>a:nth-child(2)').mouseout(function(){
-    // $('a.jz>div').css('background-color','rgba(173, 223, 248, 0.8)')
-})
-
-$('.content-left>a:nth-child(3)').mouseover(function(){
-    $('a.dx>div').css('background-color','rgba(173, 223, 248, 0.8)')
-    $('a.jz>div').css('background-color','rgba(173, 223, 248, 0.8)')
-    $('a.rj>div').css('background-color','#6bd2ff')
-    $('.content-right>.dx').hide()
-    $('.content-right>.jz').hide()
-    $('.content-right>.rj').show()
-})
+}
 
 
 
